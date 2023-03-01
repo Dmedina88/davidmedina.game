4e8623eabbfd0d86ff80b6dbab36cfc965ddc618
David Medina <dynamitemedina@gmail.com>
2023-03-01 10:58:57 -0500

    updates

--- app/src/main/java/davidmedina/game/app/features/rpg/battle/ui/EnemyView.kt
+++ app/src/main/java/davidmedina/game/app/features/rpg/battle/ui/EnemyView.kt
@@ -22,35 +22,43 @@ import kotlinx.coroutines.delay
 @Composable
 fun EnemyView(enamys: List<BattleCharacter>, onTargetSelcted: (Int) -> Unit) {
     LazyRow(Modifier.fillMaxHeight(.65f)) {
-        itemsIndexed(enamys) { int, enamy ->
-            AnimatedVisibility(visible = enamy.characterStats.isAlive) {
-                var shakeCount by remember { mutableStateOf(0) }
-                val shakeAnim by animateFloatAsState(
-                    targetValue = if (enamy.abilityBeingUsed != null || shakeCount < 3) (if (shakeCount % 2 == 0) 5f else -5f) else 0f,
-                    animationSpec = tween(durationMillis = 50)
-                )
-
-                LaunchedEffect(shakeCount) {
-                    delay(50)
-                    if (shakeCount <= 3) {
-                        shakeCount += 1
-                    } else {
-                        shakeCount = 0
-                    }
-                }
-                Image(
-                    modifier = Modifier
-                        .noRippleClickable {
-                            onTargetSelcted(int)
-                        }
-                        .graphicsLayer {
-                            rotationZ = if (enamy.abilityBeingUsed != null) shakeAnim else 0F
-                        },
-                    painter = painterResource(id = enamy.characterStats.characterID.battleImage),
-                    contentDescription = "",
-                    contentScale = ContentScale.Fit
-                )
+        itemsIndexed(enamys) { int, enemy ->
+            AnimatedVisibility(visible = enemy.characterStats.isAlive) {
+                Enamy(enemy) { onTargetSelcted(int) }
             }
         }
     }
+}
+
+@Composable
+private fun Enamy(
+    enamy: BattleCharacter,
+    onTargetSelcted: () -> Unit,
+) {
+    var shakeCount by remember { mutableStateOf(0) }
+    val shakeAnim by animateFloatAsState(
+        targetValue = if (enamy.abilityBeingUsed != null || shakeCount < 3) (if (shakeCount % 2 == 0) 5f else -5f) else 0f,
+        animationSpec = tween(durationMillis = 50)
+    )
+
+    LaunchedEffect(shakeCount) {
+        delay(50)
+        if (shakeCount <= 3) {
+            shakeCount += 1
+        } else {
+            shakeCount = 0
+        }
+    }
+    Image(
+        modifier = Modifier
+            .noRippleClickable {
+                onTargetSelcted()
+            }
+            .graphicsLayer {
+                rotationZ = if (enamy.abilityBeingUsed != null) shakeAnim else 0F
+            },
+        painter = painterResource(id = enamy.characterStats.characterID.battleImage),
+        contentDescription = "",
+        contentScale = ContentScale.Fit
+    )
 }
